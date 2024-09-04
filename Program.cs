80678a9319968111c81704f67bf3c0f5c89903e2
Katelli <aska082024@gmail.com>
2024-09-04 11:18:27 +0200

    Added two methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 namespace c_sharp_oppgave1;
 
@@ -13,13 +14,13 @@ class Program
         string myString = "Hello Everyone!";
         int myNumber = 20;
         double myDecimal = 10.25;
-        char a = 'A';
+        char l = 'A';
 
         Console.WriteLine(myString);
         Console.WriteLine(myNumber);
         Console.WriteLine(myDecimal);
         Console.WriteLine(myNumber + myDecimal);
-        Console.WriteLine(a);
+        Console.WriteLine(l);
 
         Console.WriteLine("\n");
 
@@ -47,5 +48,56 @@ class Program
         {
             Console.WriteLine(animals);
         }
+
+        Console.WriteLine("\n");
+
+        Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+        myDictionary.Add(1, "John");
+        myDictionary.Add(2, "Jane");
+        foreach (var item in myDictionary)
+        {
+            Console.WriteLine($"i: {item.Key}, id: {item.Value}");
+        }
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("The answer is");
+        Console.WriteLine(MyIntMethod(5, 3));
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Your name?");
+        string? name = Console.ReadLine();
+        if(name != null && name != ""){
+            Console.WriteLine(MyStringMethod(name));
+        } else {
+            Console.WriteLine("Please just write SOMETHING!");
+        }
+    }
+
+    static public int MyIntMethod(int a, int b)
+    {
+        int total;
+        total = a + b;
+        return total;
+    }
+
+    static public string MyStringMethod(string name)
+    {
+        switch(name)
+        {
+            case "John Snow":
+            Console.WriteLine("You know nothing " + name);
+            break;
+            case "Katrine":
+            Console.WriteLine("You know absolutely nothing " + name);
+            break;
+            case "Kai":
+            Console.WriteLine("You're amazing " + name + "!");
+            break;
+            default:
+            Console.WriteLine("You're doing great " + name);
+            break;
+        } return "Goodbye";
     }
 }
